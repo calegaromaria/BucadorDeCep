a63c5eedec08cec833dc8ba84b1a45bf551cc30c
Maria Alice Calegaro <mariaalices.calegaro@gmail.com>
2024-05-28 13:25:58 -0300

    --

--- src/GeradorDeArquivos.java
+++ src/GeradorDeArquivos.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class GeradorDeArquivos {
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class GeradorDeArquivos {
+
+    public void SalvaJson(Endereco endereco) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        FileWriter escrita = new FileWriter(endereco.cep() + "json");
+        escrita.write(gson.toJson(endereco));
+        escrita.close();
+
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Principal {
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args){
+        Scanner leitura = new Scanner(System.in);
+        ConsultaCep consultaCep = new ConsultaCep();
+        System.out.println("digite um número de CEP para a consulta");
+        var cep = leitura.nextLine();
+
+
+        try{
+            Endereco novoEndereco = consultaCep.buscaEndereco(cep);
+            System.out.println(novoEndereco);
+            GeradorDeArquivos gerador = new GeradorDeArquivos();
+            gerador.SalvaJson(novoEndereco);
+        } catch (RuntimeException | IOException e){
+            System.out.println(e.getMessage());
+            System.out.println("finalizando");
+        }
+    }
 }
